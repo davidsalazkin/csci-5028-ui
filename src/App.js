f2295f0af884c453fe11474bd46f7adbe2636860
David Salazkin <david.salazkin@gmail.com>
2023-12-15 13:03:12 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+import { Card, CardContent, Chip, Container, Typography } from '@mui/material'
 
 function App() {
+  const [articles, setArticles] = useState([])
+
+  const fetchArticleData = () => {
+    axios.get('https://csci-5028-f2a80309a443.herokuapp.com/fetch-articles')
+    .then((res) => setArticles(res.data))
+  }
+
+  const getSubjectivityChip = (subjectivityScore) => {
+    if (subjectivityScore >= 0.5) {
+      return (<Chip label="Objective" color="success" />)
+    } else {
+      return (<Chip label="Subjective" color="error" />)
+    }
+  }
+
+  const getPolarityChip = (polarityScore) => {
+    if (polarityScore >= 0.5) {
+      return (<Chip label="Positive" color="success" />)
+    } else {
+      return (<Chip label="Negative" color="error" />)
+    }
+  }
+
+  useEffect(() => {
+    fetchArticleData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ marginTop: '1em' }}>
+    {articles.map(article => {
+      return (
+        <Card style={{ marginBottom: '1em' }}>
+          <CardContent>
+            <a href={article.url} target="_blank" rel="noreferrer">
+              <Typography variant="h5">{article.title}</Typography>
+            </a>
+            {getSubjectivityChip(article.subjectivity_score)}
+            {getPolarityChip(article.polarity_score)}
+          </CardContent>
+        </Card>
+      )
+    })}
+    </Container>
   );
 }
 
